91ecf32b3ee588312cb5bb949d59727a63e8036b
zanevska <yanyazanevskaya@gmail.com>
2024-02-05 13:14:38 +0100

    test

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+const canvas = document.querySelector('#canvas'),
+  container = document.querySelector('.container'),
+  context = canvas.getContext('2d'),
+  audiofile = document.querySelector('#audiofile');
+
+let source, analyser;
+
+canvas.width = window.innerWidth;
+canvas.heigth = window.innerHeight;
+
+audiofile.addEventListener('change', function () {
+  const files = this.files;
+  const audio = document.querySelector('#audio');
+  audio.src = URL.createObjectURL(files[0]);
+  const audioContext = new AudioContext();
+  audio.load();
+  audio.play();
+
+  source = audioContext.createMediaElementSource(audio);
+  analyser = audioContext.createAnalyser();
+  source.connect(analyser);
+  analyser.connect(audioContext.destination);
+  analyser.fftSize = 64;
+  const bufferLength = analyser.frequencyBinCount;
+  let dataArray = new Uint8Array(bufferLength);
+
+  const barWidth = canvas.width / bufferLength;
+  let barHeight;
+  let x;
+  function animate() {
+    x = 0;
+    context.clearRect(0, 0, canvas.width, canvas.heigth);
+    analyser.getByteFrequencyData(dataArray);
+    for (let i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i] * 2;
+      context.fillStyle = 'black';
+      console.log(canvas.heigth - barHeight);
+      context.fillRect(x, canvas.heigth - barHeight, barWidth, barHeight);
+      x += barWidth;
+    }
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
